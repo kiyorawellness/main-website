c02092028af66f6bae83a68f73b4e77a5cd48cf1
Vvek <vivekkalola009@gmail.com>
2026-07-02 13:27:32 +0530

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kiyorawellness.github.io',
+  base: '/main-website',
   vite: {
     plugins: [tailwindcss()]
   }
