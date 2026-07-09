14f0284d9c095bd0b1c0570428a55707ada5ca09
Vivek Kalola <39058515+Vivek-Kalola@users.noreply.github.com>
2026-07-09 16:37:10 +0530

    updated astro difine to site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kiyorawellness.github.io',
-  base: '/main-website',
+  site: 'https://kiyorawellness.com',
+  base: '',
   vite: {
     plugins: [tailwindcss()]
   }
